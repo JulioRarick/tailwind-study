96482764b8d6101e8a2829e67cfe0357319c2204
Julio Rarick <juliorarick@gmail.com>
2024-06-24 15:05:44 -0300

    🎉  initial commit

---
+++ src/app/components/Sidebar/index.tsx
@@ -0,0 +1,48 @@
+import {
+  BarChart,
+  CheckSquare,
+  Cog,
+  Flag,
+  Home,
+  LifeBuoy,
+  Search,
+  SquareStack,
+  Users,
+} from 'lucide-react'
+import { Logo } from './Logo'
+import { NavItem } from './MainNavigation.tsx/NavItem'
+import { UsedSpaceWidget } from './UsedSpaceWidget'
+import { Profile } from './Profile'
+import * as Input from '../Input'
+
+export function Sidebar() {
+  return (
+    <aside className="flex flex-col gap-3 border-r border-zinc-300 px-5 py-8">
+      <Logo />
+
+      <Input.Root>
+        <Input.Prefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </Input.Prefix>
+        <Input.Control placeholder="Search" />
+      </Input.Root>
+      <div>
+        <NavItem title="Home" icon={Home} />
+        <NavItem title="Dashboard" icon={BarChart} />
+        <NavItem title="Projects" icon={SquareStack} />
+        <NavItem title="Tasks" icon={CheckSquare} />
+        <NavItem title="Reporting" icon={Flag} />
+        <NavItem title="Users" icon={Users} />
+      </div>
+      <div className="mt-auto flex flex-col gap-1">
+        <NavItem title="Support" icon={LifeBuoy} />
+        <NavItem title="Settings" icon={Cog} />
+      </div>
+
+      <UsedSpaceWidget />
+
+      <div className="h-px bg-zinc-200"></div>
+      <Profile />
+    </aside>
+  )
+}
