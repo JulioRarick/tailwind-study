4ffc4e94c5b2da516ed03d52df901851ff6fc672
Julio Rarick <juliorarick@gmail.com>
2024-06-29 00:06:33 -0300

    🚚 feat: add tailwind variants and move resources (sidebar)

--- src/app/components/Form/Select/SelectItem.tsx
+++ src/app/components/Form/Select/SelectItem.tsx
@@ -10,11 +10,11 @@ export type SelectItemProps = Select.SelectItemProps & {
 export function SelectItem({ text, ...props }: SelectItemProps) {
   return (
     <Select.Item
-      className="flex items-center gap-2 px-3 py-2.5 data-[highlighted]:bg-zinc-50"
+      className="flex items-center justify-between gap-2 px-3 py-2.5 data-[highlighted]:bg-zinc-50"
       {...props}
     >
+      <Select.ItemText className="text-black">{text}</Select.ItemText>
       <Select.ItemIndicator>
-        <Select.ItemText className="text-black">{text}</Select.ItemText>
         <Check className="h-4 w-4 text-violet-500" />
       </Select.ItemIndicator>
     </Select.Item>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import { Bold, Italic, Link, List, ListOrdered, Mail } from 'lucide-react'
-import { SettingsTabs } from '@/app/components/Form/SettingsTabs'
+import { SettingsTabs } from '@/app/components/SettingsTabs'
 import * as Input from '@/app/components/Input'
 import * as FileInput from '@/app/components/Form/FileInput'
 import { Select } from './components/Form/Select'
 import { SelectItem } from './components/Form/Select/SelectItem'
 import { Textarea } from './components/Form/Textarea'
+import { Button } from './components/Button'
 
 export default function Home() {
   return (
@@ -20,19 +21,12 @@ export default function Home() {
             </span>
           </div>
           <div className="flex items-center gap-1">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
-              type="submit"
-              form="settings"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button variant="primary" type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </div>
         <form
@@ -86,7 +80,6 @@ export default function Home() {
             <FileInput.Root className="flex items-start gap-5">
               <FileInput.ImagePreview />
               <FileInput.Trigger />
-              <FileInput.FileList />
               <FileInput.Control />
             </FileInput.Root>
           </div>
@@ -146,39 +139,27 @@ export default function Home() {
                   <SelectItem value="md" defaultChecked text="Markdown" />
                 </Select>
                 <div className="flex items-center gap-1">
-                  <button
+                  <Button
                     type="button"
                     className="rounded-md p-2 hover:bg-zinc-100"
                   >
                     <Bold className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
-                    type="button"
-                    className="rounded-md p-2 hover:bg-zinc-100"
-                  >
+                  </Button>
+                  <Button type="button" variant="ghost">
                     <Italic className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
-                    type="button"
-                    className="rounded-md p-2 hover:bg-zinc-100"
-                  >
+                  </Button>
+                  <Button type="button" variant="ghost">
                     <Link className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
-                    type="button"
-                    className="rounded-md p-2 hover:bg-zinc-100"
-                  >
+                  </Button>
+                  <Button type="button" variant="ghost">
                     <List className="h-4 w-4 text-zinc-500" strokeWidth={3} />
-                  </button>
-                  <button
-                    type="button"
-                    className="rounded-md p-2 hover:bg-zinc-100"
-                  >
+                  </Button>
+                  <Button type="button" variant="ghost">
                     <ListOrdered
                       className="h-4 w-4 text-zinc-500"
                       strokeWidth={3}
                     />
-                  </button>
+                  </Button>
                 </div>
               </div>
               <Textarea
@@ -200,22 +181,16 @@ export default function Home() {
             <FileInput.Root>
               <FileInput.Trigger />
               <FileInput.Control multiple />
+              <FileInput.FileList />
             </FileInput.Root>
           </div>
           <div className="flex items-center justify-end gap-2 pt-5">
-            <button
-              type="button"
-              className="rounded-lg border border-zinc-300 px-4 py-2 text-sm font-semibold text-zinc-700 shadow-sm hover:bg-zinc-50"
-            >
+            <Button variant="outline" type="button">
               Cancel
-            </button>
-            <button
-              type="submit"
-              form="settings"
-              className="rounded-lg bg-violet-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-violet-700"
-            >
+            </Button>
+            <Button variant="primary" type="submit" form="settings">
               Save
-            </button>
+            </Button>
           </div>
         </form>
       </div>
